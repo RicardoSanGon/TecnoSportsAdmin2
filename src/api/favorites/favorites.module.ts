87ee55d1fd7a62c65df25786a0a044f9fedda5cd
yixxus18 <yisuskroom@gmail.com>
2025-12-06 10:34:18 -0600

    fix: add User entity to FavoritesModule for UserRepository dependency

--- src/api/favorites/favorites.module.ts
+++ src/api/favorites/favorites.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
 import { Favorite } from './entities/favorite.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Favorite])],
+  imports: [TypeOrmModule.forFeature([Favorite, User])],
   controllers: [FavoritesController],
   providers: [FavoritesService],
   exports: [FavoritesService],
